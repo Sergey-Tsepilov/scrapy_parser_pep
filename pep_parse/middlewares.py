addda595f0dd12b5dfaa075758ad8e071f4c5648
Sergey Tsepilov <st08kz@yandex.kz>
2024-11-23 13:26:11 +0500

    middlewares

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -1,39 +1,35 @@
-# from itemadapter import ItemAdapter, is_item
 from scrapy import signals
 
 
-class PepParseSpiderMiddleware:
+class PepParse:
     @classmethod
     def from_crawler(cls, crawler):
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        for r in start_requests:
-            yield r
+        instance = cls()
+        crawler.signals.connect(instance.spider_opened,
+                                signal=signals.spider_opened)
+        return instance
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class PepParseDownloaderMiddleware:
-    @classmethod
-    def from_crawler(cls, crawler):
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+class PepParseSpiderMiddleware(PepParse):
+    def process_spider_input(self, response, spider):
+        return None
 
+    def process_spider_output(self, response, result, spider):
+        for item in result:
+            yield item
+
+    def process_spider_exception(self, response, exception, spider):
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        for request in start_requests:
+            yield request
+
+
+class PepParseDownloaderMiddleware(PepParse):
     def process_request(self, request, spider):
         return None
 
@@ -42,6 +38,3 @@ class PepParseDownloaderMiddleware:
 
     def process_exception(self, request, exception, spider):
         pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
